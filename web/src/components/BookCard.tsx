188f82f670c70b9743f272fdc29f02b19970982a
Tigge Nilsson <tigge.nilsson@ericsson.com>
2026-03-17 10:22:55 +0100

    Move color utility functions to a separate file

--- web/src/components/BookCard.tsx
+++ web/src/components/BookCard.tsx
@@ -1,24 +1,10 @@
 import type { Book } from '../types/Book'
+import { hashColor, ratingColor } from '../utils/colors'
 
 interface Props {
   book: Book
   onEdit: () => void
   onDelete: () => void
-}
-
-const spineColors = ['#da3633','#58a6ff','#3fb950','#d29922','#bc8cff','#f78166','#79c0ff','#56d364']
-
-function hashColor(str: string) {
-  let h = 0
-  for (let i = 0; i < str.length; i++) h = str.charCodeAt(i) + ((h << 5) - h)
-  return spineColors[Math.abs(h) % spineColors.length]
-}
-
-function ratingColor(r: number) {
-  const t = Math.max(0, Math.min(1, (r - 1) / 4))
-  const red = [248, 81, 73]
-  const blue = [88, 166, 255]
-  return `rgb(${red.map((c, i) => Math.round(c + (blue[i] - c) * t)).join(',')})`
 }
 
 export function BookCard({ book, onEdit, onDelete }: Props) {
